528179d5a9c7d77308a4d158ee0472fba44714b9
seyed0123 <seyed123ali123@gmail.com>
2023-09-30 14:09:23 +0330

    bot now active and can response to user

--- src/main.go
+++ src/main.go
@@ -1,1 +1,20 @@
-package src
+package main
+
+import (
+	"bot/bot"
+	"bot/config"
+	"fmt"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
