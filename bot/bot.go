339b2a619ba575a9742273546ec63d7330b189f2
seyed0123 <seyed123ali123@gmail.com>
2023-10-03 17:38:57 +0330

    compeleting the bot genrating message

--- bot/bot.go
+++ bot/bot.go
@@ -3,10 +3,13 @@ package bot
 import (
 	"bot/config"
 	"fmt"
-
 	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 )
 
+var count = 0
 var BotId string
 var goBot *discordgo.Session
 
@@ -45,5 +48,26 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "ping" {
 		_, _ = s.ChannelMessageSend(m.ChannelID, "pong 🏓!!")
+	} else {
+		var msg = sendMessage(m.Content)
+		_, _ = s.ChannelMessageSend(m.ChannelID, msg)
 	}
+}
+func sendMessage(message string) string {
+
+	resp, err := http.PostForm("http://localhost:8080/process",
+		url.Values{"message": {message}})
+	if err != nil {
+		fmt.Println(err)
+		return "there is an err"
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "there is an err"
+	}
+
+	return string(body)
 }
